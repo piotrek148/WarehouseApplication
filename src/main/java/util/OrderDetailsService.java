044b5a8851dc809f5a340f000b8897c4e5abd895
Andrii Dulia <av.duliya@gmail.com>
2020-09-05 09:01:49 +0100

    second commit

---
+++ src/main/java/util/OrderDetailsService.java
@@ -0,0 +1,24 @@
+package util;
+
+import model.OrderDetails;
+
+import java.util.Scanner;
+
+public class OrderDetailsService {
+
+    public OrderDetails createSinglePositionInOrder(){
+
+        OrderDetails orderDetails=new OrderDetails();
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please provide product name");
+
+
+
+
+
+
+        return null;
+
+    }
+}
